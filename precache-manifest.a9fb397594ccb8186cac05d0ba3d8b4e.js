2b90394d83824644150c8bbd1c64aaeb2f55f823
Sasha-PowerPoint <vladnovod7@gmail.com>
2023-06-25 18:54:56 +0200

    Updates

---
+++ precache-manifest.a9fb397594ccb8186cac05d0ba3d8b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e54f86a26aa731528bf0",
+    "url": "/ProxySeller/static/css/main.2b743198.chunk.css"
+  },
+  {
+    "revision": "e54f86a26aa731528bf0",
+    "url": "/ProxySeller/static/js/main.6f5bf4e3.chunk.js"
+  },
+  {
+    "revision": "182ae436e6cb183baedb",
+    "url": "/ProxySeller/static/js/runtime~main.170d7e3c.js"
+  },
+  {
+    "revision": "e4675f01f5ed8c289e8e",
+    "url": "/ProxySeller/static/css/2.1b132077.chunk.css"
+  },
+  {
+    "revision": "e4675f01f5ed8c289e8e",
+    "url": "/ProxySeller/static/js/2.2f32de8a.chunk.js"
+  },
+  {
+    "revision": "faef28047b070d7251936160b5ebbe35",
+    "url": "/ProxySeller/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ProxySeller/precache-manifest.9e941f5b6407b19e80637fabca3e9f45.js"
+  "/ProxySeller/precache-manifest.a9fb397594ccb8186cac05d0ba3d8b4e.js"
 );
 
 workbox.clientsClaim();
